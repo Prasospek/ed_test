843c8d7afa2887790975d48c482c6882e74fa48a
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-04-11 21:10:02 +0200

    First configurations of index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+import multer from "multer";
+import morgan from "morgan"
+import path from "path";
+import {fileURLToPath} from "url";
+import helmet from "helmet";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+// Middleware for parsing JSON request bodies with limit and extended options
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))) // setting directory of wheer we keep our assets
+
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "public/assets");
+    },
+    filename(req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+const upload = multer({storage});
