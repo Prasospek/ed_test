8e884ab952054f87e7241273951595d21a0303be
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-04-13 22:08:15 +0200

    Pridani themeprovider cssbaseline, importy k index.js main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,36 @@ import ProfilePage from "./scenes/profilePage";
 
 // TOHLE VSECHNO IMPORTUJI NA ZACATKU
 
+
+// PO UDELANI THEME.JS
+import {useMemo} from "react";
+import {useSelector} from "react-redux";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {createTheme} from "@mui/material";
+import {themeSettings} from "./theme";
+
+
 function App() {
+    /* PO UDELANI CSS JSEM DAL TOTO */
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+    /* PO UDELANI CSS JSEM DAL TOTO */
+
+    /* THEME PROVIDER A CSSBASELINE PRIDANY PO THEME.js */
+
     return (
         <div className="app">
             <BrowserRouter>
-                <Routes>
-                    <Route path={"/"} element={<LoginPage/>}></Route>
-                    <Route path={"/home"} element={<HomePage/>}></Route>
-                    <Route path={"/profile/:userId"} element={<ProfilePage/>}></Route>
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <Routes>
+                        <Route path={"/"} element={<LoginPage/>}></Route>
+                        <Route path={"/home"} element={<HomePage/>}></Route>
+                        <Route path={"/profile/:userId"} element={<ProfilePage/>}></Route>
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
+
         </div>
     );
 }
